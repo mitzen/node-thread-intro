36546aecd1fb70fcf169373a0b7ff1482674f4b2
jeremy woo <jeremy>
2023-09-27 20:54:36 +1300

    using axios

---
+++ axios.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+console.time("axios");
+const promises = [];
+for (let i = 0; i < 50; ++i) {
+  promises.push(execHttpRequest(i));
+}
+
+Promise.all(promises)
+.then((results) => {
+  console.log("main task", results);
+  console.timeEnd("axios");
+})
+.catch((e) => {
+  // Handle errors here
+});
+
+function execHttpRequest(i: number): Promise<void>
+{
+    return new Promise((resolve, reject) => { 
+        axios.get('https://www.google.com')
+        .then(response => {
+            console.log(response.status);
+            //resolve();
+            //resolve(response);
+        })
+        .catch(error => {
+            console.error('ops', error);
+            reject(error);
+        });
+    });
+}
+
